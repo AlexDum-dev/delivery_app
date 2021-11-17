2b7ce363bb480820129231caff630be953e5f7c7
AlexDum-dev <alexandre.dumarquez@gmail.com>
2021-11-17 23:03:24 +0100

    Ajout de la vue textuelle des request

--- src/main/java/view/test.java
+++ src/main/java/view/test.java
@@ -7,10 +7,15 @@ import javax.swing.*;
 import delivery.model.CheckPoint;
 import delivery.model.CheckPointType;
 import delivery.model.Intersection;
+import delivery.model.Plan;
 import delivery.model.Request;
+import delivery.model.XMLParser;
+import delivery.model.XMLParserException;
 
 import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.time.format.DateTimeParseException;
 import java.awt.event.ActionListener;
 
 import java.awt.event.ActionEvent;
@@ -21,11 +26,15 @@ import javax.swing.JTextField;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.DefaultTableModel;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
 
 public class test {
 
 	private JFrame frame;
 	private JTable table;
+	private Plan plan;
 
 	/**
 	 * Launch the application.
@@ -47,7 +56,9 @@ public class test {
 	 * Create the application.
 	 */
 	public test() {
+		plan = new Plan();
 		initialize();
+		
 	}
 
 	/**
@@ -58,6 +69,10 @@ public class test {
 		frame.setBounds(100, 100, 866, 562);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(526, 12, 328, 447);
+		frame.getContentPane().add(scrollPane);
+		
 		JButton btnLoadMap = new JButton("Load Map");
 		btnLoadMap.setBounds(12, 12, 134, 27);
 		btnLoadMap.addActionListener(new ActionListener() {
@@ -66,6 +81,13 @@ public class test {
 				int response = fileChooser.showOpenDialog(null);
 				if(response == JFileChooser.APPROVE_OPTION) {
 					File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+					try {
+						XMLParser.loadPlan(file, plan);
+					} catch (NumberFormatException | DateTimeParseException | ParserConfigurationException | IOException
+							| SAXException | XMLParserException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 					System.out.println(file);
 				}
 			}
@@ -82,6 +104,32 @@ public class test {
 				int response = fileChooser.showOpenDialog(null);
 				if(response == JFileChooser.APPROVE_OPTION) {
 					File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+					try {
+						XMLParser.loadRequests(file, plan);
+						
+						frame.getContentPane().add(scrollPane);
+						
+						table = new JTable();
+						
+						table.setModel(new DefaultTableModel(
+								
+								RequestView.displayRequest(plan.getRequests()), 
+							new String[] {
+								"PickUp", "Delivery", "PTime", "DTime"
+							}
+						) {
+							Class[] columnTypes = new Class[] {
+								String.class, String.class, Long.class, Long.class
+							};
+							public Class getColumnClass(int columnIndex) {
+								return columnTypes[columnIndex];
+							}
+						});
+						scrollPane.setViewportView(table);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 					System.out.println(file);
 				}
 			}
@@ -100,51 +148,13 @@ public class test {
 		panel.setBounds(10, 72, 458, 382);
 		frame.getContentPane().add(panel);
 		
+		
+		
 		//test Ajout de tableau :
 		/*JPanel TextGrid = new JPanel();
 		GridLayout gl = new GridLayout(25,40);
 		TextGrid.setLayout(gl);
 		*/
-		Intersection inter = new Intersection("1234", 3, 4);
-		Intersection inter2 = new Intersection("12345", 3, 4);
-		Intersection inter3 = new Intersection("123456", 3, 4);
-		CheckPoint pick = new CheckPoint(CheckPointType.PICKUP,inter,10);
-		CheckPoint deliv = new CheckPoint(CheckPointType.DELIVERY,inter2,10);
 		
-		CheckPoint pick1 = new CheckPoint(CheckPointType.PICKUP,inter,10);
-		CheckPoint deliv1 = new CheckPoint(CheckPointType.DELIVERY,inter2,10);
-		
-		CheckPoint pick2 = new CheckPoint(CheckPointType.PICKUP,inter,10);
-		CheckPoint deliv2 = new CheckPoint(CheckPointType.DELIVERY,inter3,10);
-		
-		Request req = new Request(deliv,pick);
-		Request req1 = new Request(deliv,pick);
-		Request req2 = new Request(deliv,pick);
-		
-		List<Request> listReq = new ArrayList();
-		listReq.add(req);listReq.add(req1);listReq.add(req2);
-		
-	
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(480, 12, 365, 65);
-		frame.getContentPane().add(scrollPane);
-		
-		table = new JTable();
-		table.setModel(new DefaultTableModel(
-				
-				RequestView.displayRequest(listReq), 
-			new String[] {
-				"PickUp", "Delivery", "PTime", "DTime"
-			}
-		) {
-			Class[] columnTypes = new Class[] {
-				String.class, String.class, Long.class, Long.class
-			};
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
-			}
-		});
-		scrollPane.setViewportView(table);
 	}
 }
