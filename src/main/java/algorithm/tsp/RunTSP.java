1337ee00ebb5e604dde3efe0b7b9698b28111174
Hugo91160 <73596098+Hugo91160@users.noreply.github.com>
2021-11-22 20:54:38 +0100

    Wip

--- src/main/java/algorithm/tsp/RunTSP.java
+++ src/main/java/algorithm/tsp/RunTSP.java
@@ -5,18 +5,7 @@ public class RunTSP {
 		TSP tsp = new TSP1();
 		for (int nbVertices = 8; nbVertices <= 16; nbVertices += 2){
 			System.out.println("Graphs with "+nbVertices+" vertices:");
-			
-			
-			int[][] costs = new int[nbVertices][nbVertices];
-			for (int i = 0; i<costs[0].length; i++) {
-				for (int j = 0; j<costs[i].length; j++) {
-					costs[i][j] = (int) (Math.random()*10);
-					if (i==j) {
-						costs[i][j] = -1;
-					}
-				}
-			}
-			Graph g = new CompleteGraph(5);
+			Graph g = new CompleteGraph(nbVertices);
 			long startTime = System.currentTimeMillis();
 			tsp.searchSolution(20000, g);
 			System.out.print("Solution of cost "+tsp.getSolutionCost()+" found in "
