4017418a6a0d2ba366f8a733031b9436a722a05d
momodeveloper <momodeveloper7@gmail.com>
2021-11-22 13:06:59 +0100

    correcting XMLParserTest

--- src/test/java/delivery/XMLParserTest.java
+++ src/test/java/delivery/XMLParserTest.java
@@ -17,8 +17,7 @@ public class XMLParserTest {
 	@Test
 	public void loadPlanTestOk() throws NumberFormatException, ParserConfigurationException, IOException, SAXException, XMLParserException {
 		//Arrange
-		ArrayList<Segment> segments = new ArrayList<>();
-		Map<String, Intersection> intersections = new HashMap<>();
+		ArrayList<Intersection> intersections = new ArrayList<>();
 		
 		Intersection i1 = new Intersection("208769457", 45.760174, 4.877455);
 		Intersection i2 = new Intersection("208769499", 45.760597, 4.87622);
@@ -27,57 +26,32 @@ public class XMLParserTest {
 		Intersection i5 = new Intersection("26033277", 45.756165, 4.8574095);
 		Intersection i6 = new Intersection("975886496", 45.756874, 4.8574047);
 		
-		intersections.put("208769457",i1);
-		intersections.put("208769499",i2);
-		intersections.put("55474978",i3);
-		intersections.put("55475018",i4);
-		intersections.put("26033277",i5);
-		intersections.put("975886496",i6);
-		
-		//We need to be careful about writing a double like this
-		segments.add(new Segment(i2, i1, 106.73056, "Rue Frédéric Passy"));
-		segments.add(new Segment(i2, i3, 79.801414, "Rue Édouard Aynard"));
-		segments.add(new Segment(i2, i4, 96.57731, "Rue Édouard Aynard"));
-		segments.add(new Segment(i6, i5, 78.72686, "Rue Danton"));
+		intersections.add(i1);
+		intersections.add(i2);
+		intersections.add(i3);
+		intersections.add(i4);
+		intersections.add(i5);
+		intersections.add(i6);
 		
 		Plan p = new Plan();
-		//Act
 		
+		//Act
 		XMLParser.loadPlan(new File("src/test/resources/XMLParserTest/loadPlanGoodDataTest.xml"),p);
 		
 		
 		//Asset
 		assertEquals(intersections.size(), p.getIntersections().size());
-		assertEquals(segments.size(), p.getSegments().size());
 		
 		boolean intersectionsCorrect=true;
-		boolean segmentsCorrect=true;
 		
-		for (String key : intersections.keySet()) {
-			if (!p.getIntersections().get(key).equals(intersections.get(key))) {
+		for (Intersection i : intersections) {
+			if (!p.getIntersection(i.getId()).equals(i)) {
 				intersectionsCorrect=false;
 			}	
 		}
 		
-		if (segments.size()!=p.getSegments().size()) {
-			segmentsCorrect=false;
-		}
-		for (int i=0; i<segments.size(); i++) {
-			boolean found=false;
-			for (int j=0; j<segments.size(); j++) {
-				if (p.getSegments().get(i).equals(segments.get(j))) {
-					found=true;
-				}	
-			}
-			if (!found) {
-				segmentsCorrect=false;
-			}
-		}
 		assertTrue(intersectionsCorrect);
-		assertTrue(segmentsCorrect);
 		
-		
-	
 	}
 	
 	@Test
@@ -103,10 +77,6 @@ public class XMLParserTest {
 	@Test
 	public void loadRequestsTestOk() throws NumberFormatException, ParserConfigurationException, IOException, SAXException, XMLParserException {
 		//Arrange
-		Plan p = new Plan();
-
-		ArrayList<Segment> segments = new ArrayList<>();
-		Map<String, Intersection> intersections = new HashMap<String, Intersection>();
 		
 		Intersection i1 = new Intersection("208769457", 45.760174, 4.877455);
 		Intersection i2 = new Intersection("208769499", 45.760597, 4.87622);
@@ -115,21 +85,8 @@ public class XMLParserTest {
 		Intersection i5 = new Intersection("26033277", 45.756165, 4.8574095);
 		Intersection i6 = new Intersection("975886496", 45.756874, 4.8574047);
 		
-		intersections.put("208769457",i1);
-		intersections.put("208769499",i2);
-		intersections.put("55474978",i3);
-		intersections.put("55475018",i4);
-		intersections.put("26033277",i5);
-		intersections.put("975886496",i6);
 		
-		//We need to be careful about writing a double like this
-		segments.add(new Segment(i2, i1, 106.73056, "Rue Frédéric Passy"));
-		segments.add(new Segment(i2, i3, 79.801414, "Rue Édouard Aynard"));
-		segments.add(new Segment(i2, i4, 96.57731, "Rue Édouard Aynard"));
-		segments.add(new Segment(i6, i5, 78.72686, "Rue Danton"));
-		
-		//Requests
-		Depot d = new Depot(i1,LocalTime.of(8, 0, 0));	
+		CheckPoint d = new CheckPoint(CheckPointType.DEPOT,i1,LocalTime.of(8, 0, 0));	
 		
 		List<Request> requests = new ArrayList<>();
 		requests.add(
@@ -145,14 +102,14 @@ public class XMLParserTest {
 				)
 		);
 		
+		Plan p = new Plan();
 		
 		//Act
 		XMLParser.loadPlan(new File("src/test/resources/XMLParserTest/loadRequestGoodDataPlanTest.xml"), p);
 		XMLParser.loadRequests(new File("src/test/resources/XMLParserTest/loadRequestGoodDataTest.xml"), p);
 		
 		
-		//Asset
-				
+		//Asset		
 		assertEquals(requests.size(), p.getRequests().size());
 		assertTrue(d.equals(p.getDepot()));
 		
